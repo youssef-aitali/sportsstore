5908fb1b10c5b8c674207083e2e6eb5b11995675
Youssef AIT ALI <youssef.aitali@outlook.com>
2020-10-08 06:10:55 +0100

    adding toggle styling to product categories, adding add to cart button and cart summary in the navigation bar

--- src/shop/CategoryNavigation.js
+++ src/shop/CategoryNavigation.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
 
     render() {
         return <React.Fragment>
-                    <Link className="btn btn-secondary btn-block"
-                        to={ this.props.baseUrl }>
+                    <ToggleLink to={ this.props.baseUrl } exact >
                     All
-                    </Link>
+                    </ToggleLink>
 
                     { this.props.categories && this.props.categories.map(cat =>
-                        <Link className="btn btn-secondary btn-block" key={ cat }
+                        <ToggleLink key={ cat }
                             to={ `${this.props.baseUrl}/${cat.toLowerCase()}`}>
                             { cat }
-                        </Link>
+                        </ToggleLink>
                     )}
                 </React.Fragment>
     }
--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CategoryNavigation } from "./CategoryNavigation";
 import { ProductList } from "./ProductList";
+import { CartSummary } from "./CartSummary";
 
 export class Shop extends Component {
 
@@ -9,6 +10,7 @@ export class Shop extends Component {
                       <div className="row">
                             <div className="col bg-dark text-white">
                                 <div className="navbar-brand">SPORTS STORE</div>
+                                <CartSummary { ...this.props } />
                             </div>
                       </div>
                       <div className="row">
@@ -17,7 +19,8 @@ export class Shop extends Component {
                                     categories={ this.props.categories } />
                             </div>
                             <div className="col-9 p-2">
-                                <ProductList products={ this.props.products } />
+                                <ProductList products={ this.props.products }
+                                             addToCart={ this.props.addToCart } />
                             </div>
                       </div>
                     </div>
